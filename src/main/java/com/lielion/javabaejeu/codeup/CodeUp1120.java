788278354431232e02990c30248ea904d51c05d2
baejeu12 <baejeu@naver.com>
2023-04-28 14:22:57 +0900

    feat : CodeUp 1119~1121 추가

---
+++ src/main/java/com/lielion/javabaejeu/codeup/CodeUp1120.java
@@ -0,0 +1,19 @@
+package com.lielion.javabaejeu.codeup;
+
+import java.util.*;
+
+public class CodeUp1120 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int input1 = sc.nextInt();
+        int input2 = sc.nextInt();
+        int input3 = sc.nextInt();
+
+        System.out.printf("%.2f",(input1 + input2 + input3) /(double)3 );
+
+
+
+    }
+}
