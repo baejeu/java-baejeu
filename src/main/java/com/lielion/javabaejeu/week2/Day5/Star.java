11df50b2132f4d3c2747069c8316ad1c49d3f5bd
baejeu12 <baejeu@naver.com>
2023-04-28 13:17:25 +0900

    feat : 프로그래머스 직사각형 별 그리기 추가

---
+++ src/main/java/com/lielion/javabaejeu/week2/Day5/Star.java
@@ -0,0 +1,27 @@
+package com.lielion.javabaejeu.week2.Day5;
+
+import java.util.Scanner;
+public class Star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input1 = sc.nextInt();
+        int input2 = sc.nextInt();
+        Solution.starPrint(input1, input2);
+
+    }
+
+    class Solution {
+        static void starPrint (int a, int b){
+
+            for (int i = 0; i < b; i++) {
+                for (int j = 0; j < a; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        }
+
+
+    }
+}
