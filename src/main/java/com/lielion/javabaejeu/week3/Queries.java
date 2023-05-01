7eca920c708e8e9bf1b9c4b23fee68200fa480a0
baejeu12 <baejeu@naver.com>
2023-05-01 12:53:09 +0900

    feat : 프로그래머스 수열과 구간 쿼리3 추가

---
+++ src/main/java/com/lielion/javabaejeu/week3/Queries.java
@@ -0,0 +1,38 @@
+package com.lielion.javabaejeu.week3;
+
+import java.util.*;
+
+public class Queries {
+    public static void main(String[] args) {
+        int [] arr = {0,1,2,3,4};
+        int [][] queries = {{0,3}, {1,2}, {1,4}};
+        Solution.solution(arr, queries);
+        System.out.println(Arrays.toString(arr));
+
+        int test = queries.length;
+       // System.out.println(test);
+
+
+
+    }
+    // [0, 1, 2, 3, 4]	[[0, 3],[1, 2],[1, 4]]	[3, 4, 1, 0, 2]
+    class Solution {
+        public static int[] solution(int[] arr, int[][] queries) {
+
+            int a =0;
+            int b =0;
+            int temp;
+
+            for (int i = 0; i < queries.length ; i++) {
+                    int j= 0;
+                    a = queries[i][j];
+                    b = queries[i][j+1];
+                    temp = arr[a];
+                    arr[a] = arr[b];
+                    arr[b] = temp;
+
+            }
+            return arr;
+        }
+    }
+}
