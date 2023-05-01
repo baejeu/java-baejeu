eeb10acb428f8c60b0999e89d53bbf3253295f60
baejeu12 <baejeu@naver.com>
2023-05-01 10:58:45 +0900

    feat : 프로그래머스 이어 붙인 수 추가

---
+++ src/main/java/com/lielion/javabaejeu/week3/NumberOfConcatenations.java
@@ -0,0 +1,33 @@
+package com.lielion.javabaejeu.week3;
+
+import java.util.*;
+
+public class NumberOfConcatenations {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int solution(int[] num_list) {
+
+            int size = num_list.length;
+            String answer2 ="";
+            String answer3 ="";
+            int sum =0;
+            int answer = 0;
+            for (int i = 0; i < size ; i++) {
+
+                if (num_list[i]%2==0){
+                    answer2 = answer2 + String.valueOf(num_list[i]);
+                }
+                else {
+                    answer3 = answer3 + String.valueOf(num_list[i]);
+                }
+            }
+
+            answer = Integer.parseInt(answer2)+Integer.parseInt(answer3);
+
+            return answer;
+        }
+    }
+}
