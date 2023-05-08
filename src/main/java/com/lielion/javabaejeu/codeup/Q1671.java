4bc02a22db4602c2e997f7161b781cbef453d3bd
baejeu12 <baejeu@naver.com>
2023-05-08 15:15:31 +0900

    feat : 코드업 1671 이길때까지 하는 죽음의 가위바위보 게임

---
+++ src/main/java/com/lielion/javabaejeu/codeup/Q1671.java
@@ -0,0 +1,53 @@
+package com.lielion.javabaejeu.codeup;
+/*
+1671 : 가위바위보 게임
+(※ 바위=0, 가위=1, 보=2를 말한다.)
+1. 바위(0)는 가위(1)를 이긴다.
+
+2. 가위(1)는 보(2)를 이긴다.
+
+3. 보(2)는 바위(0)를 이긴다.
+
+4. 같은 것을 내면 비긴다.
+
+win, tie, lose를 출력한다. 결과는 광현이 중심으로 낸다.
+ */
+
+import java.util.*;
+public class Q1671 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        do {
+            int human = sc.nextInt();
+            int computer = sc.nextInt();
+            String answer = RockPaperScissors(human, computer);
+            if (answer == "win") break;
+            System.out.println("you win");
+        }
+        while(true);
+
+    }
+
+    public static String RockPaperScissors (int human , int computer){
+        if (human==0){ // 0은 바위 1은 가위 2는 보
+            if (computer == 0) return "tie";
+            else if (computer == 1) return "win";
+            else if (computer == 2) return "lose";
+        }
+        else if (human==1){
+            if (computer == 0) return "lose";
+            else if (computer == 1) return "tie";
+            else if (computer == 2) return "win";
+
+        }
+        else{ // human == 2
+            if (computer == 0) return "win";
+            else if (computer == 1) return "lose";
+            else if (computer == 2) return "tie";
+        }
+
+        return "";
+    }
+
+}
